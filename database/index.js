7497750f3035f952640f58cee47f83b8390fd0a4
almeigab <gabrielalmeida1403@hotmail.com>
2022-04-29 00:54:32 -0300

    added params to db url

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL);
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 
 const db = mongoose.connection;
